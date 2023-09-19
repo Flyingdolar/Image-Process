793a8cf979068c9a226dcdfa883aea29b9c07b78
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-09-19 16:43:29 +0800

    chore[windiCSS]: Add CSS tools

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,4 +1,10 @@
-const path = require('path');
+const path = require("path");
+
 module.exports = {
-  outputDir: "../web" // or whatever you would like your output directory to be, which will be used in the next step.
-}
+  outputDir: "../web",
+  pluginOptions: {
+    windicss: {
+      // see https://github.com/windicss/vite-plugin-windicss/blob/main/packages/plugin-utils/src/options.ts
+    },
+  },
+};
