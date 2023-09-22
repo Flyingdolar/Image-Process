54f2bd5293eb77d93f599fa0313b98d899385307
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-09-22 16:58:09 +0800

    feat: add 404 page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "@/views/HomeView.vue";
+import NotFound from "@/views/NotFound.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -7,6 +8,7 @@ const routes: Array<RouteRecordRaw> = [
     name: "home",
     component: HomeView,
   },
+  { path: "*", name: "404", component: NotFound },
 ];
 
 const router = createRouter({
