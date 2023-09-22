451133e40a753273fec08c67cd04508554f1f415
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-09-22 14:56:06 +0800

    env: Remove unused output path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
-const path = require("path");
-
 module.exports = {
-  outputDir: "../web",
   pluginOptions: {
     windicss: {
       // see https://github.com/windicss/vite-plugin-windicss/blob/main/packages/plugin-utils/src/options.ts
