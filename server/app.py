4722c429ad36f535b99a1a5bfc0e9f26f3d7e6cd
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-09-24 22:47:39 +0800

    Remove all stuff

--- server/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request, jsonify
-
-app = Flask(__name__) # Flask app instance
-
-@app.route('/', methods=['POST']) # Route
-def index():
-    return jsonify({'message': 'Hello, World!'}) # Return JSON with a key of 'message' and value of 'Hello, World!'
-
-app.run() # Start the Flask app
